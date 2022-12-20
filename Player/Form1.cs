7c77e5821ea668cdf2f3fc62bcc0ca06f16eb4c8
dokki <dokki@LAPTOP-HK12KLH9>
2022-12-20 16:28:35 +0900

    초기 커밋.

---
+++ Player/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = @"C:\Users\dokki\OneDrive\바탕 화면\깨나라영상\2_2022-12-16_10-43-59_089.mkv";
+        }
+    }
+}
---
+++ Player/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Player
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = @"C:\Users\dokki\OneDrive\바탕 화면\깨나라영상\2_2022-12-16_10-43-59_089.mkv";
+            openFileDialog1.Filter = "Video File | *.mkv;";
+
+            //axWindowsMediaPlayer1.URL = @"C: \Users\dokki\OneDrive\바탕 화면\깨나라영상\2_2022 - 12 - 16_10 - 43 - 59_089.mkv";
+        }
+    }
+}
